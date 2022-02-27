672711fd9ac1e67b200c9f24daa6bdf65a600d92
Richard <richard.tarassov@hotmail.com>
2022-02-27 11:59:16 +0200

    Initial commit

---
+++ src/test/java/finalcountdown/homerecipesystembackend/HomeRecipeSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package finalcountdown.homerecipesystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeRecipeSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
