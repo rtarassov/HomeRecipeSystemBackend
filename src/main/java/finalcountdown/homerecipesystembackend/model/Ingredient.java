c3f9a6930031a3deb176762db62e717039e7719f
Richard <richard.tarassov@hotmail.com>
2022-02-28 19:52:14 +0200

    [change] - CRUD features for Ingredient

--- src/main/java/finalcountdown/homerecipesystembackend/model/Ingredient.java
+++ src/main/java/finalcountdown/homerecipesystembackend/model/Ingredient.java
@@ -1,11 +1,15 @@
 package finalcountdown.homerecipesystembackend.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ingredient {
 
     @Id
