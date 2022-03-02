74cf4ac29a1132d921f9e23f1120a19b0844b6e5
Richard <richard.tarassov@hotmail.com>
2022-03-02 19:53:16 +0200

    [add/change] - added previous methods back that got lost in merge

--- src/main/java/finalcountdown/homerecipesystembackend/repository/RecipeRepository.java
+++ src/main/java/finalcountdown/homerecipesystembackend/repository/RecipeRepository.java
@@ -2,7 +2,9 @@ package finalcountdown.homerecipesystembackend.repository;
 
 import finalcountdown.homerecipesystembackend.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
 }
--- src/main/java/finalcountdown/homerecipesystembackend/service/RecipeService.java
+++ src/main/java/finalcountdown/homerecipesystembackend/service/RecipeService.java
@@ -1,13 +1,20 @@
 package finalcountdown.homerecipesystembackend.service;
 
 import finalcountdown.homerecipesystembackend.dto.RecipeIngredientRequest;
+import finalcountdown.homerecipesystembackend.model.Recipe;
 import finalcountdown.homerecipesystembackend.repository.IngredientRepository;
 import finalcountdown.homerecipesystembackend.repository.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@Slf4j
 public class RecipeService {
 
     private final RecipeRepository recipeRepository;
@@ -27,5 +34,36 @@ public class RecipeService {
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+    }
+
+    public void saveRecipe(Recipe recipeEntity) {
+        log.info("recipe entity for saving", recipeEntity);
+        recipeRepository.save(recipeEntity);
+    }
+
+    public List<Recipe> readAllRecipes() {
+        log.info("Trying to read all recipes in database");
+        var result = recipeRepository.findAll();
+        log.info("Recipes saved from DB: " + result);
+        return result;
+    }
+
+    public Optional<Recipe> readRecipeById(Long id) {
+        log.info("trying to read recipe by id: [{}]", id);
+        var maybeRecipe = recipeRepository.findById(id);
+        log.info("found Ingredient: [{}]", maybeRecipe);
+        return maybeRecipe;
+    }
+
+    @Transactional
+    public boolean deleteRecipeById(Long id) {
+        log.info("Trying to delete recipe entity by id: [{}]", id);
+
+        boolean result = false;
+        if (recipeRepository.existsById(id)) {
+            recipeRepository.deleteById(id);
+            result = true;
+        }
+        return result;
     }
 }
